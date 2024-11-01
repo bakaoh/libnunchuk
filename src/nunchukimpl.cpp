ff918f1513d89298e815da08c05dd88fe181cf1d
Bakaoh <bakaoh@gmail.com>
2024-11-01 13:45:52 +0700

    Add LocalDB

--- src/nunchukimpl.cpp
+++ src/nunchukimpl.cpp
@@ -1114,6 +1114,7 @@ Transaction NunchukImpl::SignTransaction(const std::string& wallet_id,
           }
         }
         signed_psbt = software_signer.SignTaprootTx(
+            {chain_, {}, "/home/bringer/libnunchuk/examples/playground.cpp/tmp/local.sqlite", {}},
             psbt, basepath, wallet.get_descriptor(DescriptorPath::EXTERNAL_ALL),
             wallet.get_descriptor(DescriptorPath::INTERNAL_ALL),
             storage_->GetCurrentAddressIndex(chain_, wallet_id, false),
@@ -1189,6 +1190,7 @@ Transaction NunchukImpl::SignTransaction(const Wallet& wallet,
           }
         }
         signed_psbt = software_signer.SignTaprootTx(
+            {chain_, {}, "/home/bringer/libnunchuk/examples/playground.cpp/tmp/local.sqlite", {}},
             psbt, basepath, wallet.get_descriptor(DescriptorPath::EXTERNAL_ALL),
             wallet.get_descriptor(DescriptorPath::INTERNAL_ALL),
             storage_->GetCurrentAddressIndex(chain_, wallet.get_id(), false),
